9b9dc1eee1e6633cca0493d735fca511bb57662f
Liam Heisler <Liam Heisler>
2016-10-17 21:11:13 -0400

    Updates

--- src/intervivew/Main.java
+++ src/intervivew/Main.java
@@ -14,12 +14,16 @@ public class Main {
 	//Instance variables used...initialized later.
 	public static String name; 
 	public static int age; 
+	public static int ageSwitch; 
 	public static int n; 
+	public static int numApplicants; 
 	public static boolean isMale; 
 	
+	static TheInterview person = new TheInterview(); 
+	
 	//Main method.
-	public static void main(String[] args) {
-		TheInterview person = new TheInterview(); 
+	public static void main(String[] args) {a
+		numApplicants = 0; 
 		
 		//Execute code specified...if error is found, execute catch() 
 		try {
@@ -27,22 +31,39 @@ public class Main {
 			while(true) {
 				System.out.println(" \t Apply for membership to the club..");
 				System.out.println(" \t ----------------------------------");
+			
+				name = person.getName();
+				age = person.getAge(); 
 				
-				//Count members of the team.
-				for(int x = 0; x < 8; x++) {
-					name = person.getName();
-					age = person.getAge(); 
-				
-					isMale = person.getSex(); 
+				isMale = person.getSex(); 
 					
-					//Is the person above 14 and less than 18, are the M/F?
-					if(age == -1 && isMale == false) {					
-						x++; 
-						System.out.println("Congratulations, " + name + 
-								" you are member number " + x + 
-								" to join the team!");
-					}
+				//Is the person above 14 and less than 18, are the M/F?
+				/*
+				if(age == -1 && isMale == false) {		
+					numApplicants++; 
+					//System.out.println("Congratulations, " + name + 
+					//		" you are member number " + x + 
+					//		" to join the team!");
 				}
+				*/
+				
+				/*
+				ageSwitch = 0; 
+				switch(age) {
+					//Age is between 14 and 18.
+					case 1:
+						ageSwitch += 1;
+					//Age is less than 14.
+					case 2:
+						ageSwitch += 2;
+						System.out.println("We ask you to join the FIRST Lego League.");
+					//Age is greater than 18. 
+					case 3:
+						ageSwitch += 3;
+						System.out.println("We ask you to join a club at your University.");
+				}
+				*/
+				
 			}
 		
 		}
@@ -52,5 +73,19 @@ public class Main {
 		}
 		
 		//Add any extra error's found here (catch{} statements).
+	}
+	
+	public static void getAgePart2() {
+		age = person.getAge(); 
+		ageSwitch = 0; 
+		
+		switch(age) {
+			case 1:
+				ageSwitch += 1; 
+			case 2:
+				ageSwitch += 2;
+			case 3:
+				ageSwitch += 3; 
+		}
 	}
 }
--- src/intervivew/TheInterview.java
+++ src/intervivew/TheInterview.java
@@ -1,26 +1,37 @@
 package intervivew;
 
-import java.util.InputMismatchException;
+/*
+ * This class handles most of the interviewing process. There are
+ * separate methods designed to handle the data entered by the user
+ * (i.e. sex, age, name)...
+ */
+
+//import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TheInterview {
+	//Our scanner, this is where the information comes from.
 	static Scanner sc = new Scanner(System.in); 
 	
+	//Instance variables...initialized later.
 	public static String name; 
 	public int age;
 	public boolean isMale; 
 	
 	int n = 0; 
 	
+	//Method to return the name of the person being interviewed.
 	public String getName() {
 		System.out.println("Enter your name: ");
 		name = sc.nextLine();
 		return name; 
 	}
+	//Method to return the age of the person being interviewed.
 	public int getAge() {
 		System.out.println("Enter your age, in years: ");
 		age = sc.nextInt(); 
 		
+		/*
 		if(age < 14) {
 			System.out.println("You must join FIRST Lego League.");
 			return -1; 
@@ -31,24 +42,34 @@ public class TheInterview {
 		} else {
 			return age; 
 		}
+		*/
+		
+		//Used in switch/case statement in the Main class. 
+		if(age > 14 && age < 18) {
+			return 1;
+		}
+		else if(age < 14) {
+			return 2;
+		}
+		else if(age > 18) {
+			return 3; 
+		} else {
+			return 4; //Useless (in a way)
+		}
 	}
+	//Method to handle the sex of the person being interviewed.
 	public boolean getSex() {
-		try {
-			System.out.println("Are you a male? Enter 'true' or 'false.'");		
-			isMale = sc.nextBoolean(); 
+		System.out.println("Are you a male? Enter 'true' or 'false.'");		
+		isMale = sc.nextBoolean(); 
 		
-			if(isMale == false) {
-				System.out.println("Sorr, you must join the BambiBotz!");
-				return true; 
-			}
-			else if(isMale == true) {
-				return true; 
-			}
+		if(isMale == false) {
+			System.out.println("Sorr, you must join the BambiBotz!");
+			return true; 
 		}
-		catch(InputMismatchException ex) {
-			System.out.println("Please enter 'true' or 'false'!");
+		else if(isMale == true) {
+			return true; 
+		} else {
+			return true; 
 		}
-		
-	    return true; 
 	}
 }
